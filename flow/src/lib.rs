6ed26e049ad5769cfc5c2f669eaabfe83f101743
waku <wakunguma13@gmail.com>
2025-05-26 07:50:46 +0200

    refactor: move layout to `flow` crate

--- flow/src/lib.rs
+++ flow/src/lib.rs
@@ -1,14 +1,20 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+//! General purpose layout engine
+//! 
+
+pub trait Layout{
+
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub enum MainAxisAlignment{
+    Start,
+    Center,
+    End,
+    SpaceEvenly,
+    SpaceBetween
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub enum CrossAxisAlignment{
+    Start,
+    Center,
+    End
 }
--- ruby/src/widget/mod.rs
+++ ruby/src/widget/mod.rs
@@ -1,7 +1,7 @@
 mod circle;
 mod rect;
 mod column;
-use std::{fmt::Debug, random::random};
+use std::fmt::Debug;
 pub use column::{Column};
 pub use rect::Rect;
 pub use circle::Circle;
@@ -14,38 +14,6 @@ pub trait Widget: Debug {
     fn children(&self) -> Vec<Box<dyn Widget>>{
         vec![]
     }
-}
-
-#[derive(Debug,Clone, Copy,PartialEq, Eq, PartialOrd, Ord)]
-pub struct WidgetId(pub i64);
-
-impl WidgetId{
-    pub fn new() -> Self { 
-        Self::default() 
-    }
-}
-
-impl Default for WidgetId {
-    fn default() -> Self {
-        // TODO replace with rand crate
-        let id: i64 = random();
-        Self(id)
-    }
-}
-
-
-pub enum MainAxisAlignment{
-    Start,
-    Center,
-    End,
-    SpaceEvenly,
-    SpaceBetween
-}
-
-pub enum CrossAxisAlignment{
-    Start,
-    Center,
-    End
 }
 
 pub enum WidgetState{
