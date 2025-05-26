6a512bfc8b9d8f86657bd8e3e412523b58f36fc1
waku <wakunguma13@gmail.com>
2025-05-26 07:36:04 +0200

    feat: add widget id's to sync between different structures

--- src/widget/mod.rs
+++ src/widget/mod.rs
@@ -1,8 +1,8 @@
 mod circle;
 mod rect;
 mod column;
-use std::fmt::Debug;
-pub use column::Column;
+use std::{fmt::Debug, random::random};
+pub use column::{Column};
 pub use rect::Rect;
 pub use circle::Circle;
 use tiny_skia::Pixmap;
@@ -11,7 +11,28 @@ use tiny_skia::Pixmap;
 pub trait Widget: Debug {
     /// Draw the [`Widget`] onto the screen.
     fn paint(&self,pixmap: &mut Pixmap);
+    fn children(&self) -> Vec<Box<dyn Widget>>{
+        vec![]
+    }
 }
+
+#[derive(Debug,Clone, Copy,PartialEq, Eq, PartialOrd, Ord)]
+pub struct WidgetId(pub i64);
+
+impl WidgetId{
+    pub fn new() -> Self { 
+        Self::default() 
+    }
+}
+
+impl Default for WidgetId {
+    fn default() -> Self {
+        // TODO replace with rand crate
+        let id: i64 = random();
+        Self(id)
+    }
+}
+
 
 pub enum MainAxisAlignment{
     Start,
